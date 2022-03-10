e8ca74de2dbc4e4bca230157c9b143c3d9990a59
Redevectored <redevectored@protonmail.com>
2022-03-10 13:08:14 +0100

    Colors for shield- and hp-texts

---
+++ Assets/HealthAndShieldUI.cs
@@ -0,0 +1,71 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthAndShieldUI : MonoBehaviour
+{
+    [Range(0f, 1000f)]
+    public int healthPoints;
+    public Text healthPointsText;
+
+    float[] shieldsPoints = new float[4];
+    public Text[] shieldsTexts = new Text[4];
+
+    public GameObject player;
+    public DamageModel damageModel;
+
+
+    void Start()
+    {
+        player = GameObject.Find("Player");
+        damageModel = player.GetComponent<DamageModel>();
+    }
+
+ 
+    void Update()
+    {
+        healthPointsText.text = "HP: " + damageModel.Hull.ToString("0");
+
+        damageModel.Shields(shieldsPoints);
+
+        setColorOfHPAndShieldTexts();
+        
+    }
+
+    void setColorOfHPAndShieldTexts()
+    {
+        if (damageModel.Hull >= 2/3 * damageModel.maxHull)
+        {
+            healthPointsText.color = Color.green;
+        }
+        else if (damageModel.Hull >= 1/3 * damageModel.maxHull)
+        {
+            healthPointsText.color = Color.yellow;
+        }
+        else
+        {
+            healthPointsText.color = Color.red;
+        }
+
+        for (int i = 0; i < shieldsPoints.Length; i++)
+        {
+            shieldsTexts[i].text = "Shield " + (i + 1) + ": " + shieldsPoints[i].ToString("0");
+
+            if (shieldsPoints[i] >= 2/3 * damageModel.maxShield[i])
+            {
+                shieldsTexts[i].color = Color.green;
+            }
+            else if (shieldsPoints[i] >= 1/3 * damageModel.maxShield[i])
+            {
+                shieldsTexts[i].color = Color.yellow;
+            }
+            else
+            {
+                shieldsTexts[i].color = Color.red;
+            }
+
+        }
+    }
+}
