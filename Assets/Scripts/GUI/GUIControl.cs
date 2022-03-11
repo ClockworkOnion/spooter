16f94330697d62ff4c78a736e0dbe7dc95d5e5d2
ClockworkOnion <happella@students.uni-marburg.de>
2022-03-11 17:08:41 +0100

    CameraTilt, Energy display, Enemy Guns

--- Assets/Scripts/GUI/GUIControl.cs
+++ Assets/Scripts/GUI/GUIControl.cs
@@ -6,25 +6,38 @@ public class GUIControl : MonoBehaviour
 {
 
     private PlayerDamageModel dmgModel;
+    private ShipSystems plrShipSystems;
     private ShieldRingDisplay[] shieldsDisplay = new ShieldRingDisplay[4];
     private float[] currentShields = new float[4]; // vorne, rechts, hinten, links
     private float[] maxShields = new float[4];
 
+    private float maxEnergy = 100;
+    private float currentEnergy = 50;
+    private ShieldRingDisplay energyRings;
+
     void Start()
     {
+        plrShipSystems = GameObject.FindGameObjectWithTag("Player").GetComponent<ShipSystems>();
         dmgModel = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerDamageModel>();
         shieldsDisplay[0] = transform.Find("ShieldForward").GetComponent<ShieldRingDisplay>();
         shieldsDisplay[1] = transform.Find("ShieldRight").GetComponent<ShieldRingDisplay>();
         shieldsDisplay[2] = transform.Find("ShieldBackward").GetComponent<ShieldRingDisplay>();
         shieldsDisplay[3] = transform.Find("ShieldLeft").GetComponent<ShieldRingDisplay>();
-
         dmgModel.Shields(currentShields);
         maxShields = dmgModel.maxShield;
+        energyRings = transform.Find("EngineRings").GetComponent<ShieldRingDisplay>();
+
 
     }
 
     void Update()
     {
+        // Energy pool
+        maxEnergy = plrShipSystems.maxEnergyPool;
+        currentEnergy = plrShipSystems.Energy;
+        energyRings.SetPercentages(currentEnergy, maxEnergy);
+
+        // Shields
         dmgModel.Shields(currentShields);
         for (int i = 0; i < 4; i++)
         {
---
+++ Assets/Scripts/Weapons/Launcher/EnemyLaserCannon.cs
@@ -0,0 +1,45 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyLaserCannon : AimedWeapon
+{
+    public float firingDistance = 10f;
+    Transform playerTransform;
+    // Start is called before the first frame update
+    protected override void Start()
+    {
+        shipBody = GetComponentInParent<Rigidbody2D>();
+        playerTransform = GameObject.FindGameObjectWithTag("Player").GetComponent<Transform>();
+    }
+
+
+    protected override bool Refire()
+    {
+        return (lastShot += Time.deltaTime) > refireRate && Vector3.Distance(playerTransform.position, transform.position) < firingDistance;
+    }
+
+    protected override bool Powered()
+    {
+        return true;
+    }
+
+    protected override float FiringAngle()
+    {
+        return Vector3.SignedAngle(playerTransform.position - transform.position, Vector3.up, Vector3.forward);
+    }
+
+    protected override bool CanFire(float angle)
+    {
+        angle = Vector3.SignedAngle(playerTransform.transform.position - transform.position, transform.up, Vector3.forward);
+        for (int i = 0; i < firingAngle.Length; ++i)
+        {
+            if (angle > firingAngle[i] & angle < firingAngle[++i])
+            {
+                return true;
+            }
+        }
+        return false;
+    }
+
+}
