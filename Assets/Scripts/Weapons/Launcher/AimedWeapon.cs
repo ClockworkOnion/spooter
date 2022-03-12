a1443b22c56c77059f68667e012140e6d0d28f0b
Jahnph <Jahnph@students.uni-marburg.de>
2022-03-12 18:40:17 +0100

    weapon clamp

--- Assets/Scripts/Weapons/Launcher/AimedWeapon.cs
+++ Assets/Scripts/Weapons/Launcher/AimedWeapon.cs
@@ -18,19 +18,37 @@ public abstract class AimedWeapon : Weapon
 
     protected override bool CanFire(float angle)
     {
-        angle = Vector3.SignedAngle(crosshair.transform.position - transform.position, transform.up, Vector3.forward);
-        for (int i = 0; i < firingAngle.Length; ++i)
-        {
-            if (angle > firingAngle[i] & angle < firingAngle[++i])
-            {
-                return true;
-            }
-        }
-        return false;
+        return true;
     }
 
     protected override float FiringAngle()
     {
-        return Vector3.SignedAngle(crosshair.transform.position - transform.position, Vector3.up, Vector3.forward);
+        float angle = Vector2.SignedAngle(crosshair.transform.position - transform.position, Vector2.up);
+        float angleToShip = Vector2.SignedAngle(crosshair.transform.position - transform.position, transform.up);
+        for (int i = 1; i < firingAngle.Length - 1; i += 2)
+        {
+            if(angleToShip > firingAngle[i] && angleToShip < firingAngle[i + 1])
+            {
+                if(Mathf.DeltaAngle(firingAngle[i], angleToShip) < Mathf.DeltaAngle(angleToShip, firingAngle[i + 1]))
+                {
+                    return angle - Mathf.DeltaAngle(firingAngle[i], angleToShip);
+                } else
+                {
+                    return angle + Mathf.DeltaAngle(angleToShip, firingAngle[i + 1]);
+                }
+            }
+        }
+        if(angleToShip < firingAngle[0] || angleToShip > firingAngle[firingAngle.Length - 1])
+        {
+            if (Mathf.DeltaAngle(firingAngle[firingAngle.Length - 1], angleToShip) < Mathf.DeltaAngle(angleToShip, firingAngle[0]))
+            {
+                return angle - Mathf.DeltaAngle(firingAngle[firingAngle.Length - 1], angleToShip);
+            }
+            else
+            {
+                return angle + Mathf.DeltaAngle(angleToShip, firingAngle[0]);
+            }
+        }
+        return angle;
     }
 }
