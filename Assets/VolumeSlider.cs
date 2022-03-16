d64cb6ed2ac48e9ef78c9dc6ce3f76e5fd3920e7
Redevectored <redevectored@protonmail.com>
2022-03-16 14:01:57 +0100

    Add part of endscene, full main menu volume slider

--- Assets/VolumeSlider.cs
+++ Assets/VolumeSlider.cs
@@ -1,10 +1,9 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class MainMenu : MonoBehaviour
+public class VolumeSlider : MonoBehaviour
 {
     [SerializeField]
     Slider volSlider;
@@ -16,32 +15,10 @@ public class MainMenu : MonoBehaviour
         {
             PlayerPrefs.SetFloat("musicVolume", 1);
             Load();
-        }
-        else
+        } else
         {
             Load();
         }
-    }
-
-    public void PlayGame()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    }
-
-    public void PlayTheStorySoFar()
-    {
-        SceneManager.LoadScene(0);
-    }
-
-    public void PlayCredits()
-    {
-        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 2);
-    }
-
-    public void QuitGame()
-    {
-        Application.Quit();
-        Debug.Log("Quitting Game..");
     }
 
     public void ChangeVolume()
